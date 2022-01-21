047700a0be1e35b254836cd4021ba74aae85dabb
Aly Westwood <alymak.w@gmail.com>
2022-01-21 10:49:13 -0500

    deposit stuff

--- client/src/pages/client/Deposit.js
+++ client/src/pages/client/Deposit.js
@@ -20,26 +20,47 @@ function Deposit() {
   // const { payeeAccountId, payerAccountId, value, chequeNumber } = tempData;
 
 
-  const [file, setFile] = useState(null);
+//   const [file, setFile] = useState(null);
   const [accountList, setAccountList] = useState([]);
 
   useEffect(() => {
     axios.get('/accounts/totals', reqHeader)
     .then(res => setAccountList(res.data.accountTotals))
     .catch(err => console.log(err.response.data));
-  }, [])
+  }, []);
 
-  const submitFile = async () => {
-    try {
-      if (!file) {
-        throw new Error("Select a file first!");
-      }
-      const formData = new FormData();
-      formData.append("file", file[0]);
-      await axios.post(`/transactions/depositCheque`, formData, {
+  const submitFile = submit => {
+      submit.preventDefault();
+
+      try {
+        if (!submit.target.files) {
+          throw new Error("Select a file first!");
+        }
+        } catch (error) {
+        // handle error
+        console.log(error.data)
+        }
+        const formData = new FormData();
+        formData.append('file', submit.target.file.files[0]);
+
+        // console.log(submit.target.file.files)
+        // var form = {
+        //     payeeAccountId: submit.target.payeeAccount.value,
+        //     payerAccountId: submit.target.payerAccount.value,
+        //     value: submit.target.value.value,
+        //     chequeNumber: submit.target.chequeNumber.value,
+        //     file: submit.target.file.files[0]
+        // }
+      formData.append('payeeAccountId', submit.target.payeeAccount.value);
+      formData.append('payerAccountId', submit.target.payerAccount.value);
+      formData.append('value', submit.target.value.value);
+      formData.append('chequeNumber', submit.target.chequeNumber.value);
+
+    //   formData.append("file", file[0]);
+      axios.post(`/transactions/depositCheque`, formData, {
         headers: {
           "Content-Type": "multipart/form-data",
-          accessToken: localStorage.getItem("access-token"),
+          accessToken: localStorage.getItem("accessToken"),
         },
       }).then((result) => {
         console.log(result);
@@ -47,23 +68,19 @@ function Deposit() {
         console.log(err.response.data)
       })
       // handle success
-    } catch (error) {
-      // handle error
-      console.log(error.data)
-    }
   };
 
   return (
     <Card className='col-7'>
         <Card.Header>Deposit a cheque</Card.Header>
         <Card.Body>
-        <Form>
+        <Form onSubmit={submitFile}>
         <Row>
         <Col>
         <Form.Group controlId='payeeAccount'>
             <Form.Label>Account</Form.Label>
-            <Form.Select defaultValue={null}>
-                <option value={null} disabled>Select</option>
+            <Form.Select defaultValue='null' onChange={(event) => {}}>
+                <option value='null' disabled>Select</option>
                 {accountList.map(account => {
                     return <option key={account.accountId} value={account.accountId}>Account #{account.accountId} - {account.currency} {account.accountType}</option>
                 })}
@@ -87,17 +104,17 @@ function Deposit() {
         <Col>
         <Form.Group controlId='chequeNumber'>
             <Form.Label>Cheque S/N</Form.Label>
-            <Form.Control/>
+            <Form.Control />
         </Form.Group>
         </Col>
         </Row>
         <Row>
         <Form.Group controlId='file' className='mb-3'>
             <Form.Label>Upload image</Form.Label>
-            <Form.Control type="file" onChange={(event) => setFile(event.target.files)} />
+            <Form.Control type="file" />
         </Form.Group>
         </Row>
-        <Button onClick={submitFile}>Send</Button>
+        <Button type='submit'>Send</Button>
         </Form>
         </Card.Body>
     </Card>
