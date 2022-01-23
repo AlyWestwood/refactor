7760497b7e7cbddfdacb1236006fd1fc90cc17f1
Aly Westwood <alymak.w@gmail.com>
2022-01-23 12:31:22 -0500

    scrum

--- client/src/pages/admin/Accounts.js
+++ client/src/pages/admin/Accounts.js
@@ -1,7 +1,52 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Card, Table } from 'react-bootstrap';
+import { reqHeader } from '../../misc/reqHeader';
+import DisplayAccounts from '../../components/DisplayAccounts';
+
+
 
 function Accounts() {
-  return <div></div>;
+  const [accounts, setAccounts] = useState([]);
+
+  useEffect(() => {
+    axios.get('/admin/approveCreditAccounts', reqHeader)
+    .then(res => {
+      setAccounts(res.data.listOfInactiveCreditAccounts);
+      console.log(res.data.listOfInactiveCreditAccounts)
+    })
+    .catch(err => console.log(err))
+  }, [])
+
+  return (
+      <>
+        <Card>
+          <Card.Header>
+
+          </Card.Header>
+          <Card.Body>
+            <Table>
+              <thead>
+
+              </thead>
+              <tbody>
+          {accounts.map(account => {
+            return(
+                <tr>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+              )
+            })}
+              </tbody>
+            </Table>
+          </Card.Body>
+        </Card>
+
+      </>
+    );
 }
 
 export default Accounts;
